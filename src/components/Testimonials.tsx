b42859708b02e805ea6d2a7f72b5786f84db0ced
DoctorTone <tony@drt-software.com>
2024-10-24 11:32:41 +0100

    Click through testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid2";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import testimonials from "../data/testimonials";
 
 const Testimonials = () => {
+  const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
+  const previousTestimonial = () => {
+    if (currentTestimonial === 0) {
+      setCurrentTestimonial(testimonials.length - 1);
+    } else {
+      setCurrentTestimonial(currentTestimonial - 1);
+    }
+  };
+
+  const nextTestimonial = () => {
+    if (currentTestimonial === testimonials.length - 1) {
+      setCurrentTestimonial(0);
+    } else {
+      setCurrentTestimonial(currentTestimonial + 1);
+    }
+  };
+
   return (
-    <Container>
-      <Typography variant="h4" sx={{ mt: 5, mb: 5, color: "#386fc7" }}>
-        Testimonials
-      </Typography>
+    <Container sx={{ mt: 5, mb: 5 }}>
       <Grid container>
-        <Grid size={2}>
-          <ArrowBackIosIcon />
+        <Grid size={2} sx={{ textAlign: "center" }}>
+          <ArrowBackIosIcon onClick={previousTestimonial} />
         </Grid>
         <Grid size={8} sx={{ textAlign: "center" }}>
-          <Typography variant="h5">Tony was great to work with!</Typography>
+          <Typography variant="h5">
+            {testimonials[currentTestimonial].text}
+          </Typography>
         </Grid>
-        <Grid size={2}>
-          <ArrowForwardIosIcon />
+        <Grid size={2} sx={{ textAlign: "center" }}>
+          <ArrowForwardIosIcon onClick={nextTestimonial} />
         </Grid>
       </Grid>
     </Container>
---
+++ src/data/testimonials.ts
@@ -0,0 +1,12 @@
+const testimonials = [
+  {
+    id: 0,
+    text: "Tony was great to work with!",
+  },
+  {
+    id: 2,
+    text: "Mental health trials",
+  },
+];
+
+export default testimonials;
