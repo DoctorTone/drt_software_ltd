f23ecdc7a9b7e3dbb85fdf47d0bbde7d99d69e18
DoctorTone <tony@drt-software.com>
2024-10-24 16:40:33 +0100

    Updated projects text

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,7 +27,9 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">Virtual Art Gallery</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              Virtual Art Gallery
+            </Typography>
             <Typography variant="body1">
               You can view realistic art pieces, such as a single painting or a
               gallery where collections are situated in adjoining rooms,
@@ -51,20 +53,15 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">3D Data Visualisation</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              3D Data Visualisation
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              Data visualisation does not have to be confined to two dimensions,
+              we can use the additional space of 3D to express the information
+              more effectively. I have visualised data from many fields
+              including climate, sports, finance, medical, financial and many
+              others.
             </Typography>
           </Grid>
           <Grid size={{ xs: 12, md: 6 }}>
@@ -91,20 +88,22 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">3D Websites</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              3D Websites
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              Websites built with 3D technology can open up a whole new world of
+              possibilities right in your browser: ✨Enhanced engagement -
+              interact with 3D models, feel more involved. ✨Improved
+              visualisation - see realistic products from any angle.
+              ✨Innovative branding - 3D websites tell your story differently.
+              ✨Increased visitors - engaging and interactive sites encourage
+              visitors, who are more tempted to stay. ✨Compelling experiences -
+              highly interactive sites provide increased depth and context.
+              ✨Competitive edge - your site will have an edge over your
+              competitors. The enhanced look and feel of your website will make
+              you stand out from the crowd, reinforcing your message whilst
+              looking pretty impressive!
             </Typography>
           </Grid>
         </Grid>
@@ -115,20 +114,14 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">3D Configurators</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              3D Configurators
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              I have built a range of product configurators for several sectors
+              such as the bathroom, furniture and construction industries; each
+              one allowing customers to preview products before they make a
+              purchase.
             </Typography>
           </Grid>
           <Grid size={{ xs: 12, md: 6 }}>
@@ -155,20 +148,16 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">Physics and Simulation</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              Physics and Simulation
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              The web isn't just confined to visualisations, it's also capable
+              of the realistic simulation of many things from solar systems to
+              atomic structures. Here are a couple of examples: ### The Solar
+              System This demo was built for schools to help visualise the solar
+              system - to get a sense of how far away everything is and how big
+              the planets are, especially in relation to the Earth.
             </Typography>
           </Grid>
         </Grid>
@@ -179,20 +168,13 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">Spline Components</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              Spline Components
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              While building several websites in Spline, I created a set of
+              reusable components. This component shows a collection of animated
+              text effects and materials.
             </Typography>
           </Grid>
           <Grid size={{ xs: 12, md: 6 }}>
@@ -219,20 +201,21 @@ const Projects = () => {
             <Typography variant="h5" color="#386fc7">
               Projects
             </Typography>
-            <Typography variant="h3">3D Medical Visualisation</Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              3D Medical Visualisation
+            </Typography>
             <Typography variant="body1">
-              You can view realistic art pieces, such as a single painting or a
-              gallery where collections are situated in adjoining rooms,
-              potentially allowing the gallery to go on forever! The artefacts
-              looked stunning as they were 3D scans of the originals. This
-              allowed artists to display and potentially sell their artwork from
-              anywhere in the world. Alternatively, it allows famous pieces of
-              art to be put on display so that you can get a chance to view the
-              Mona Lisa or a Constable up close! For the ultimate sense of
-              realism, you can view the pieces in VR - seeing the pieces through
-              a headset takes the experience to another level! The project is
-              still ongoing and has already recruited artists to exhibit on the
-              platform. You can get a sense of the experience below:
+              Medical imaging software can be very complex and expensive, with a
+              steep learning curve. We wanted to create an application that
+              could be used to measure and monitor tumour lesions that was
+              simple to use and gave accurate results. In conjunction with a
+              neurosurgeon, I built an application that took a collection of 2D
+              medical images and created a 3D volume. This can then be used to
+              visualise anatomic structures and identify areas of interest. The
+              software allowed specified areas to be isolated, and the
+              corresponding data could then be analysed and any cancerous
+              lesions were identified and measured. This then allowed monitoring
+              of the tumour to determine the effectiveness of any treatments.
             </Typography>
           </Grid>
         </Grid>
--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -9,8 +9,16 @@ const TopSection = () => {
       <Typography variant="h2" sx={{ mt: 3 }}>
         Hi, I'm Tony
       </Typography>
-      <Typography variant="h5" sx={{ mt: 3 }}>
-        I'm passionate about building software that makes a difference.
+      <Typography variant="h6" sx={{ mt: 3 }}>
+        I'm passionate about all things 3D and the capabilities that 3D
+        technology can do for you and your business. I don't just build
+        pretty-looking websites and applications, I help accelerate your
+        business impact and recognition by showing off your products in a new
+        light, with the latest cutting-edge technology. Your customers will love
+        it, your competitors will be jealous!
+      </Typography>
+      <Typography variant="h6" sx={{ mt: 3 }}>
+        Here are a collection of projects that showcase some of my work.
       </Typography>
     </Container>
   );
