45f0849bc9b7124e5ea320561fad4b3527c911da
DoctorTone <tony@drt-software.com>
2024-10-24 11:10:18 +0100

    Updated projects format and new image

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Img = styled("img")({
   display: "block",
@@ -12,28 +13,38 @@ const Img = styled("img")({
 const Projects = () => {
   return (
     <Container>
-      <Typography variant="h4" sx={{ mt: 3, mb: 2, color: "#386fc7" }}>
-        Projects
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid size={6}>
-          <div className="bordered">
-            <Img src="./images/effects.jpg" alt="projects" />
-            <Typography className="hoverText" variant="h4">
-              Virtual Art Gallery
+      <Box sx={{ mt: 5, background: "#404040", borderRadius: "20px" }}>
+        <Grid container spacing={2} sx={{ mt: 3, padding: "30px" }}>
+          <Grid size={6}>
+            <div className="bordered">
+              <Img src="./images/museum.jpg" alt="projects" />
+              <Typography className="hoverText" variant="h4">
+                Virtual Art Gallery
+              </Typography>
+            </div>
+          </Grid>
+          <Grid size={6} sx={{ textAlign: "center" }}>
+            <Typography variant="h5" color="#386fc7">
+              Projects
             </Typography>
-          </div>
+            <Typography variant="h3">Virtual Art Gallery</Typography>
+            <Typography variant="body1">
+              You can view realistic art pieces, such as a single painting or a
+              gallery where collections are situated in adjoining rooms,
+              potentially allowing the gallery to go on forever! The artefacts
+              looked stunning as they were 3D scans of the originals. This
+              allowed artists to display and potentially sell their artwork from
+              anywhere in the world. Alternatively, it allows famous pieces of
+              art to be put on display so that you can get a chance to view the
+              Mona Lisa or a Constable up close! For the ultimate sense of
+              realism, you can view the pieces in VR - seeing the pieces through
+              a headset takes the experience to another level! The project is
+              still ongoing and has already recruited artists to exhibit on the
+              platform. You can get a sense of the experience below:
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid size={6}>
-          <Img src="./images/effects.jpg" alt="projects" />
-        </Grid>
-        <Grid size={6}>
-          <Img src="./images/effects.jpg" alt="projects" />
-        </Grid>
-        <Grid size={6}>
-          <Img src="./images/effects.jpg" alt="projects" />
-        </Grid>
-      </Grid>
+      </Box>
     </Container>
   );
 };
