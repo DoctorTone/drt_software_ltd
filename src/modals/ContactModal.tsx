7a0a0f73dd0d4be5df77795e015814c285680257
DoctorTone <tony@drt-software.com>
2024-10-30 20:04:59 +0000

    Fixed ts errors

--- src/modals/ContactModal.tsx
+++ src/modals/ContactModal.tsx
@@ -1,23 +1,8 @@
-import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  color: "black",
-  width: 340,
-  bgcolor: "background.paper",
-  border: "6px solid blue",
-  borderRadius: "20px",
-  boxShadow: 24,
-  p: 4,
-};
 
 interface ContactModalProps {
   open: boolean;
