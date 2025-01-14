6d85b0eddf41c9b4d6e3de3d83c076b830f5fc87
DoctorTone <tony@drt-software.com>
2025-01-14 11:56:54 +0000

    Updated images for data viz projects

--- src/components/DataProjects.tsx
+++ src/components/DataProjects.tsx
@@ -78,7 +78,7 @@ const DataProjects = () => {
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <Img src="./images/portfolio.jpg" alt="projects" />
+                <Img src="./images/finance.jpg" alt="projects" />
               </a>
               {!isMobile && (
                 <Typography className="hoverText" variant="h4">
@@ -138,14 +138,18 @@ const DataProjects = () => {
               sx={{ textAlign: "justify" }}
             >
               <p>
-                This example depicts sleep patterns for a subject over a number
-                of months, illustrating all the associated attributes. The
-                application is fully interactive allowing individual attribute
-                selection, scaling and highlighting of any relevant trends.
+                During the global pandemic, data visualisation played a key role
+                in providing information and analysis of the virus. The huge
+                quantities of data generated have been utilsied by both
+                scientific and governmental bodies to help inform policy
+                decisions.
               </p>
               <p>
-                The extra space allows all the attributes to be separated,
-                allowing for much more effective visualisation.
+                They also provide the general public with a means to scrutinise
+                all the information. This visualisation shows the virus data for
+                the first wave in the UK. It separates out aspects of the data
+                so that they may be studied more easily, yet still allows
+                comparisons to be made.
               </p>
             </Typography>
             <Box sx={{ textAlign: "center" }}>
@@ -164,7 +168,7 @@ const DataProjects = () => {
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <Img src="./images/dataViz.jpg" alt="projects" />
+                <Img src="./images/covid.jpg" alt="projects" />
               </a>
               {!isMobile && (
                 <Typography className="hoverText" variant="h4">
@@ -172,6 +176,58 @@ const DataProjects = () => {
                 </Typography>
               )}
             </div>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ mt: 5, background: "#404040", borderRadius: "20px" }}>
+        <Grid container spacing={2} sx={{ mt: 3, padding: "30px" }}>
+          <Grid
+            size={{ xs: 12, md: 6 }}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <div className="bordered">
+              <a
+                href="https://drt-software.com/Portfolio/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <Img src="./images/horror.jpg" alt="projects" />
+              </a>
+              {!isMobile && (
+                <Typography className="hoverText" variant="h4">
+                  3D Data Visualisation
+                </Typography>
+              )}
+            </div>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }} sx={{ textAlign: "center" }}>
+            <Typography variant="h5" color="#386fc7">
+              Data Visualisation
+            </Typography>
+            <Typography variant="h3" sx={{ mb: 3 }}>
+              Real Time
+            </Typography>
+            <Typography
+              variant="body1"
+              className="paddingProjects"
+              sx={{ textAlign: "justify" }}
+            >
+              Processing data over the web makes it an ideal platform to be able
+              to visualise data in real-time. At the Mayhem Horror festival we
+              visualised the EEG activity of film fans as they watched the
+              horror! Made in conjunction with colleagues at Thrill Laboratory
+              and the Mixed Reality Lab.
+            </Typography>
+            <Box sx={{ textAlign: "center" }}>
+              <Button
+                href="https://drt-software.com/Portfolio/"
+                target="_blank"
+                rel="noreferrer noopener"
+                variant="contained"
+              >
+                Go to Demo
+              </Button>
+            </Box>
           </Grid>
         </Grid>
       </Box>
